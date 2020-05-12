109397101307aeb4ad90653d9ce6839eb88feee7
Boluwatifeh <tifeasypeasy@gmail.com>
2020-05-11 18:53:51 -0700

    Initial commit

---
+++ app/src/main/java/com/example/lipetodo/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.lipetodo;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> arrayAdapter;
+    String messageText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        arrayList = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+
+    }
+
+    public void onClick(View v){
+        Intent intent= new Intent();
+        intent.setClass(MainActivity.this, EditFieldClass.class);
+        startActivityForResult(intent, Intent_Constants.INTENT_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Intent_Constants.INTENT_REQUEST_CODE){
+            messageText = data.getStringExtra(Intent_Constants.INTENT_MESSAGE_FIELD);
+            arrayList.add(messageText);
+            arrayAdapter.notifyDataSetChanged();
+
+        }
+    }
+}
